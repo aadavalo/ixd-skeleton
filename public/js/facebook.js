0bb0f0ff9494d87a9300659e2bf430f650c8231d
Juan Zapien <jmzapien@ucsd.edu>
2018-02-22 22:35:46 -0800

    turing in

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -22,7 +22,7 @@ function statusChangeCallback(response) {
 function changeUser(response) {
     $('.facebookLogin').hide();
     $('.next').css("visibility", "visible");
-    if(location != "http://localhost:3000/index"){
+    if(location != "https://bustle.herokuapp.com/index"){
       $(location).attr("href",'/index');
     }
     $('h5').text(response.name);
